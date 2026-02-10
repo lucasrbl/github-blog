3309352b03268fdaaf539bf144cb30b5674330b9
Lucas Ribeiro Bonfilio de Lemos <lucasrblemos@gmail.com>
2026-02-10 08:12:02 -0300

    feat: add bg-image to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,30 @@
 import { Card, Header, Input } from "../components";
+import coverImage from "../assets/cover.png";
 
 export function Home() {
     return (
-        <div className="max-w-360 mx-75 my-50 flex flex-col">
-            <Header />
+        <>
+            <div style={{ backgroundImage: `url(${coverImage})` }} className="h-[300px] w-full bg-cover bg-center"></div>
+            <div className="max-w-360 mx-75 -mt-24 mb-50 flex flex-col m-">
+                <Header />
 
-            <div className="mb-12 mt-[70px]">
-                <div className="mb-3 flex justify-between">
-                    <h2 className="text-title-small text-base-subtitle">Publicações</h2>
-                    <span className="text-base-span text-small">6 publicações</span>
+                <div className="mb-12 mt-[70px]">
+                    <div className="mb-3 flex justify-between">
+                        <h2 className="text-title-small text-base-subtitle">Publicações</h2>
+                        <span className="text-base-span text-small">6 publicações</span>
+                    </div>
+                    <Input placeholder="Buscar conteúdo" className="w-full mb-8" />
                 </div>
-                <Input placeholder="Buscar conteúdo" className="w-full mb-8" />
-            </div>
 
-            <div className="grid grid-cols-2 gap-8">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-            </div>
+                <div className="grid grid-cols-2 gap-8">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
         </div>
+        </>
     )
 }
