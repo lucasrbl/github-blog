38e8df02ead5b4d574797d380fd8c779f5cefb15
Lucas Ribeiro Bonfilio de Lemos <lucasrblemos@gmail.com>
2026-02-10 08:12:27 -0300

    feat: update card text length

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,14 +2,14 @@ type CardProps = {} & React.ComponentProps<'div'>;
 
 export function Card({ ...props }: CardProps) {
     return ( 
-    <div className="bg-base-post rounded-[10px] border-2 border-transparent hover:border-2 hover:border-base-label cursor-pointer w-full group" {...props} >
+    <div className="bg-base-post rounded-[10px] border-2 border-transparent hover:border-2 hover:border-base-label cursor-pointer group" {...props} >
         <div className="m-8">
             <div className="mb-11.5 flex justify-between">
                 <span className="text-base-title text-link transition-all group-hover:text-medium">JavaScript data types and data structures</span>
                 <span className="text-base-span text-small">Há 1 dia</span>
             </div>
 
-            <p className="text-base-text text-medium">Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.</p>
+            <p className="text-base-text text-medium line-clamp-4">Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.</p>
         </div>
     </div> 
     )
